72473929855b79f9247ca16d61d81ebebd05eac9
NureDanchenkoDenys <denys.danchenko@nure.ua>
2026-04-01 02:26:01 +0300

    Update code-examples-pract1.cs

--- Pract1/pzpi-23-9-danchenko-denys-pract1/code-examples-pract1.cs
+++ Pract1/pzpi-23-9-danchenko-denys-pract1/code-examples-pract1.cs
@@ -1,1 +1,72 @@
+using System;
 
+namespace FactoryMethodExample
+{
+    // Абстрактний продукт
+    abstract class Transport
+    {
+        public abstract string Deliver();
+    }
+
+    // Конкретний продукт 1
+    class Truck : Transport
+    {
+        public override string Deliver()
+        {
+            return "Доставка вантажу здiйснюється автомобiльним транспортом.";
+        }
+    }
+
+    // Конкретний продукт 2
+    class Ship : Transport
+    {
+        public override string Deliver()
+        {
+            return "Доставка вантажу здiйснюється морським транспортом.";
+        }
+    }
+
+    // Абстрактний творець
+    abstract class Logistics
+    {
+        public abstract Transport CreateTransport();
+
+        public string PlanDelivery()
+        {
+            Transport transport = CreateTransport();
+            return "Логiстична операцiя: " + transport.Deliver();
+        }
+    }
+
+    // Конкретний творець 1
+    class RoadLogistics : Logistics
+    {
+        public override Transport CreateTransport()
+        {
+            return new Truck();
+        }
+    }
+
+    // Конкретний творець 2
+    class SeaLogistics : Logistics
+    {
+        public override Transport CreateTransport()
+        {
+            return new Ship();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logistics logistics;
+
+            logistics = new RoadLogistics();
+            Console.WriteLine(logistics.PlanDelivery());
+
+            logistics = new SeaLogistics();
+            Console.WriteLine(logistics.PlanDelivery());
+        }
+    }
+}
