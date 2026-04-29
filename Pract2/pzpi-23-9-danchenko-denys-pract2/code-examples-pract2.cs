343fb8845a638cafef5625ac96c0e15f64c04c76
NureDanchenkoDenys <denys.danchenko@nure.ua>
2026-04-29 04:14:45 +0300

    Update code-examples-pract2.cs

--- Pract2/pzpi-23-9-danchenko-denys-pract2/code-examples-pract2.cs
+++ Pract2/pzpi-23-9-danchenko-denys-pract2/code-examples-pract2.cs
@@ -1,1 +1,38 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
 
+var client = new HttpClient();
+string accessToken = "USER_ACCESS_TOKEN";
+string trackId = "TRACK_ID";
+
+client.DefaultRequestHeaders.Authorization =
+    new AuthenticationHeaderValue("Bearer", accessToken);
+
+var response = await client.GetAsync(
+    $"https://api.spotify.com/v1/tracks/{trackId}");
+
+string result = await response.Content.ReadAsStringAsync();
+Console.WriteLine(result);
+
+/* */
+
+public class ListeningEvent
+{
+    public string UserId { get; set; }
+    public string TrackId { get; set; }
+    public string Action { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public async Task SendListeningEventAsync()
+{
+    var listeningEvent = new ListeningEvent
+    {
+        UserId = "user123",
+        TrackId = "track456",
+        Action = "play",
+        CreatedAt = DateTime.UtcNow
+    };
+
+    await recommendationService.ProcessEventAsync(listeningEvent);
+}
